9ff91910f6b4519db55f0cd0bd322fe89b8eb559
Vitor Gagliani <93485462+VitorGagliani@users.noreply.github.com>
2026-08-10 20:01:50 -0300

    Aula 10-08

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 6;
+        int n2 = 8;
+        int n3 = 10;
+
+
+        float media = (n1 + n2 + n3)/2;
+        System.out.println("media = " + media);
+
+    }
+}
---
+++ src/main/java/org/example/MainV2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MainV2 {
+    public static void main(String[] args) {
+        int n1 = 6;
+        int n2 = 8;
+        int n3 = 10;
+
+
+        float media = (n1 + n2 + n3) / 3;
+        System.out.println("media = " + media);
+    }
+}
